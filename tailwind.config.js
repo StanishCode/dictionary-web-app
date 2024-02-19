8ba2a647e72d0fc684999c6a38efd9f3e110fd33
Cody Stanish <112568028+StanishCode@users.noreply.github.com>
2024-02-19 14:46:16 -0500

    create inital structure and custom tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx},"],
+  theme: {
+    extend: {
+      colors: {
+        veryDarkGrey: "#050505",
+        darkGrey: "#1f1f1f",
+        veryMedGrey: "#2d2d2d",
+        medGrey: "#3a3a3a",
+        grey: "#757575",
+        lightGrey: "#e9e9e9",
+        veryLightGrey: "#f4f4f4",
+        white: "#ffffff",
+        amethyst: "#a445ed",
+        salmon: "#ff5252",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Lora", "serif"],
+        mono: ["Inconsolata", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
