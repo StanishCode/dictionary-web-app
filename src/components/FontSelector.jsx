6ad2d4fd5e039f976cf9bc34a873c19267165c80
Cody Stanish <112568028+StanishCode@users.noreply.github.com>
2024-03-04 13:23:21 -0500

    update to dynaically render font selection in font selector component and update fonts object to array of objects.

--- src/components/FontSelector.jsx
+++ src/components/FontSelector.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import FontSelectorItem from "./FontSelectorItem";
 
 function FontSelector({ onSelect, font }) {
   const [isOpen, setIsOpen] = useState(false);
-  const fonts = {
-    ["font-sans"]: "Sans-serif",
-    ["font-serif"]: "Serif",
-    ["font-mono"]: "Mono",
-  };
+  const fonts = [
+    { name: "Sans-serif", value: "font-sans" },
+    { name: "Serif", value: "font-serif" },
+    { name: "Mono", value: "font-mono" },
+  ];
+
+  const selectedFont = fonts.find((currFont) => currFont.value === font);
 
   function handleOpenFontSelect() {
     setIsOpen(true);
@@ -16,73 +19,39 @@ function FontSelector({ onSelect, font }) {
     setIsOpen(false);
   }
 
-  function handleFontSelect(event) {
-    onSelect(event.target.value);
+  function handleFontSelect(value) {
+    onSelect(value);
     handleCloseFontSelect();
   }
+
+  const everyFont = fonts.map((item, index) => {
+    return (
+      <FontSelectorItem
+        key={index + Math.random() * 1000}
+        onFontSelect={() => {
+          handleFontSelect(item.value);
+        }}
+        name={item.name}
+        value={item.value}
+      />
+    );
+  });
+
   return (
     <div className="relative max-w-24">
       <button
         onClick={handleOpenFontSelect}
         className="w-full flex justify-between items-center gap-4 text-sm dark:text-white"
       >
-        <span className="shrink-0 font-semibold">{fonts[font]}</span>
+        <span className="shrink-0 font-semibold">{selectedFont.name}</span>
         <img src="./src/assets/images/icon-arrow-down.svg" alt="" />
       </button>
-      {/* TODO dynamically render each font list item */}
       <ul
         className={`${
           isOpen ? "" : "hidden"
         } absolute right-0 w-44 p-4 flex flex-col gap-2 rounded-lg shadow-lg bg-white dark:bg-darkGrey dark:text-white dark:shadow-amethyst`}
       >
-        <li className="w-full flex">
-          <input
-            onClick={handleFontSelect}
-            className="invisible"
-            type="radio"
-            name="font"
-            id="font-sans"
-            value={"font-sans"}
-          />
-          <label
-            className="w-full font-sans font-semibold cursor-pointer"
-            htmlFor="font-sans"
-          >
-            Sans-serif
-          </label>
-        </li>
-        <li className="w-full flex">
-          <input
-            onClick={handleFontSelect}
-            className="invisible"
-            type="radio"
-            name="font"
-            id="font-serif"
-            value={"font-serif"}
-          />
-          <label
-            className="w-full font-serif font-semibold cursor-pointer"
-            htmlFor="font-serif"
-          >
-            Serif
-          </label>
-        </li>
-        <li className="w-full flex">
-          <input
-            onClick={handleFontSelect}
-            className="invisible"
-            type="radio"
-            name="font"
-            id="font-mono"
-            value={"font-mono"}
-          />
-          <label
-            className="w-full font-mono font-semi-bold cursor-pointer"
-            htmlFor="font-mono"
-          >
-            Mono
-          </label>
-        </li>
+        {everyFont}
       </ul>
     </div>
   );
---
+++ src/components/FontSelectorItem.jsx
@@ -0,0 +1,22 @@
+function FontSelectorItem({ onFontSelect, name, value }) {
+  return (
+    <li className="w-full flex">
+      <input
+        onClick={onFontSelect}
+        className="invisible"
+        type="radio"
+        name="font"
+        id={value}
+        value={value}
+      />
+      <label
+        className="w-full font-sans font-semibold cursor-pointer"
+        htmlFor={value}
+      >
+        {name}
+      </label>
+    </li>
+  );
+}
+
+export default FontSelectorItem;
